951fce6f9a75c970810d692aaea076117558cdab
Josh <joshuamares180@gmail.com>
2022-01-01 09:54:16 -0800

    Basics

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//basic idea based on dogmatika ygo universe
+/*moving with arrow keys
+  abilities with qwer
+  other things df
+  items with *button* and arrow keys
+  player stats and abilities on top left
+  enemy stats top right
+  map layout given by ascii characters in txt file
+  32x32 or 64x64
+  default char names are ecclesia and fallen of albaz
+  ecclesia
+    q:
+    w:
+    e:
+    r:Solemn strike - insta kills but also creates hole in
+      ground that takes you to next level if you fall in
+   foa
+    q:
+    w:
+    e:
+    r:branded fusion - 
+   future chars:
+    shuraig
+    alluber?
+    fluer de lis -> iris
+    kitt or ferrijit
+    maximus
+    
+   two player:
+    single player other controlled by ai
+   stages:
+    Dogmatika nation
+
+    */
+
+class Player{
+public:
+    //init fxn
+        //given from main menu
+        //main menu passes txt file with stats
+
+    string name;
+    string 
+private:
+
+};
+
+int main(){
+    cout << "print statement" << endl;
+    return 0;
+}
